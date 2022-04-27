4b07505a56b794e51bbd6e06923cf416cc41e91a
Jonas Subel <jsubel@students.42wolfsburg.de>
2022-04-27 14:49:03 +0200

    fixed norm errors

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -6,23 +6,9 @@
 /*   By: jsubel <jsubel@student.42wolfsburg.de >    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/06 14:12:38 by jsubel            #+#    #+#             */
-/*   Updated: 2022/01/17 11:16:10 by jsubel           ###   ########.fr       */
+/*   Updated: 2022/04/27 14:48:46 by jsubel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include "libft.h"
-
-/* t_list	*ft_lstlast(t_list *lst)
-{
-	t_list	*current;
-
-	if (!lst)
-		return (NULL);
-	current = lst;
-	while (current->next != NULL)
-		current = current->next;
-	return (current);
-} */
 
 #include "libft.h"
 
--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: jsubel <jsubel@student.42wolfsburg.de >    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/29 17:20:15 by jsubel            #+#    #+#             */
-/*   Updated: 2022/01/21 10:25:59 by jsubel           ###   ########.fr       */
+/*   Updated: 2022/04/27 14:48:33 by jsubel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,18 +31,4 @@ char	*ft_strmapi(const char *s, char (*f)(unsigned int, char))
 	}
 	str[i] = '\0';
 	return (str);
-}
-
-char	myfunction(unsigned int i, char c)
-{
-	if (i < 10)
-		c += i;
-	return (c);
-}
-
-int main(void)
-{
-	char	*str1 = "aaaaaaaaaaaaaaaa";
-	printf("\nTest 1: initial string\n%s\nNew string:\n%s", str1, ft_strmapi(str1, myfunction));
-	return (0);
 }
